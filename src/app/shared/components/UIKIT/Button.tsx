056f5b5e6e88a9e5dbef789b4d5c4522d83d5647
ArchLinux <host@archlinux>
2025-01-31 17:17:30 +1000

    rebased_arch

---
+++ src/app/shared/components/UIKIT/Button.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from 'react';
+import s from '../../styles/components/UIKIT/Button.module.scss';
+import classNames from 'classnames/bind';
+
+interface ButtonProps {
+  children: ReactNode;
+  size: 's' | 'm' | 'l' | 'auto';
+  style: 'black_outline' | 'black_solid' | 'white_outline' | 'white_solid';
+}
+
+const Button: FC<ButtonProps> = props => {
+  const cx = classNames.bind(s);
+  return <div className={cx(s.button, props.size, props.style)}>{props.children}</div>;
+};
+
+export default Button;
