301e2bd2bc61111bbd73bf80b0ef2ec832b3cdbf
ArchLinux <host@archlinux>
2025-02-01 13:22:31 +1000

    did mapping products

--- src/app/components/Home/HomeListProducts/HomeListProducts.tsx
+++ src/app/components/Home/HomeListProducts/HomeListProducts.tsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from 'react';
 import s from './HomeListProducts.module.scss';
 import { Container } from '@/app/shared/components/Container/Container';
 import { AxiosInterceptor } from '@/app/core/http';
-import { IProduct } from '@/app/shared/models/IProduct';
 import { Product } from '@/app/shared/components/layout/Product/Product';
+import { homeListContent } from './homeListProductsData';
+import classNames from 'classnames';
 
 export const HomeListProduct = () => {
+  const [currentCategory, setCurrentCategory] = useState<number>(0);
+  const cx = classNames.bind(s);
   useEffect(() => {
     getData();
   }, []);
@@ -18,12 +21,30 @@ export const HomeListProduct = () => {
 
   return (
     <Container>
-      <div className={s.HomeListProduct}>
-        {homeListProducts.map((i, idx) => (
-          <div key={idx}>
-            <Product title={i.title} id={i.id} img={i.img} price={i.price} isLiked={i.isLiked} />
-          </div>
-        ))}
+      <div className={s.HomeListProducts}>
+        <div className={s.HomeListProducts__selector}>
+          {homeListContent.map(i => (
+            <div
+              className={
+                currentCategory === i.id
+                  ? cx(s.HomeListProducts__selector_item, s.HomeListProducts__selector_item_active)
+                  : cx(s.HomeListProducts__selector_item)
+              }
+              key={i.id}
+              onClick={() => setCurrentCategory(i.id)}
+            >
+              {i.category}
+            </div>
+          ))}
+        </div>
+
+        <div className={s.HomeListProducts__container}>
+          {homeListContent[currentCategory].products.map((i, idx) => (
+            <div key={idx}>
+              <Product title={i.title} id={i.id} img={i.img} price={i.price} isLiked={i.isLiked} />
+            </div>
+          ))}
+        </div>
       </div>
     </Container>
   );
